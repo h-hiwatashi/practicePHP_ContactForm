3f333c6f212e76fb3dbc44ebf420d6db090f5457
EC2 Default User <ec2-user@ip-172-31-6-160.ap-northeast-1.compute.internal>
2020-06-24 00:31:33 +0000

    フォームの修正

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
 
         <div class="form-item">お問い合わせの種類</div>
         <?php 
-          $types = array('Progateに関するお問い合わせ', 'Progateに対する意見', '採用に関するお問い合わせ', '取材・メディア関連のお問い合わせ', '料金に関するお問い合わせ', 'その他');
+          $types = array('弊社に関するお問い合わせ', '弊社に対する意見', '採用に関するお問い合わせ', '取材・メディア関連のお問い合わせ', '料金に関するお問い合わせ', 'その他');
          ?>
         <select name="category">
           <option value="未選択">選択してください</option>
@@ -64,9 +64,7 @@
         <li>お問い合わせ</li>
       </ul>
     </div>
-    <div class="like-box">
-      <iframe src="https://www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FProgate%2F742679992421539&amp;show_faces=false" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:300px;" allowTransparency="true"></iframe>
-    </div>
+
   </div>
 </body>
 </html>
--- sent.php
+++ sent.php
@@ -44,9 +44,7 @@
         <li>お問い合わせ</li>
       </ul>
     </div>
-    <div class="like-box">
-      <iframe src="https://www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FProgate%2F742679992421539&amp;show_faces=false" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:300px;" allowTransparency="true"></iframe>
-    </div>
+
   </div>
 </body>
 </html>
